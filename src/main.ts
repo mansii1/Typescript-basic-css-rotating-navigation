65b62d3f1c5370df8f7ab52faace349acb0d9c6d
mansii1 <mansimangukiya0@gmail.com>
2025-08-17 00:29:24 +0530

    Done

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const open = document.getElementById("open") as HTMLButtonElement;
+const close = document.getElementById("close") as HTMLButtonElement;
+const container = document.querySelector(".container") as HTMLDivElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+open.addEventListener("click", () => {
+  console.log("open clicked");
+  container.classList.add("show-nav");
+});
+
+close.addEventListener("click", () => {
+  console.log("close clicked");
+  container.classList.remove("show-nav");
+});
